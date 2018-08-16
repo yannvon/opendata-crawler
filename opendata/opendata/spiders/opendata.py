2332cc26d9b43a82f33ff6b623fa163bfebb8982
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2018-08-16 13:16:37 +0200

    First version saving metadata

--- opendata/opendata/spiders/opendata.py
+++ opendata/opendata/spiders/opendata.py
@@ -15,6 +15,7 @@ class QuotesSpider(scrapy.Spider):
             'https://opendata.swiss/en/dataset?res_format=CSV&page=1',
             'https://opendata.swiss/en/dataset?res_format=CSV&page=2',
         ]
+
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
@@ -27,18 +28,35 @@ class QuotesSpider(scrapy.Spider):
             )
 
     def parse_article(self, response):
-        filter_title = ''.join(filter(lambda s: s != '/n', title))
+        path = response.url.split('/')[-1]
+
+        # First yield metadata file
+        meta_href = response.css("a:contains('API (JSON)')::attr(href)").extract_first()
+
+        yield Request(
+            url=response.urljoin(meta_href),
+            callback=self.save_meta, meta={'dir': path}
+        )
 
         for href in response.css('li.resource-item a[href$=".csv"]::attr(href)').extract():
             print("Article found: %s", href)
             yield Request(
                 url=response.urljoin(href),
-                callback=self.save_csv, meta={'title': filter_title}
+                callback=self.save_csv, meta={'dir':  path}
             )
+
+    def save_meta(self, response):
+        path = response.url.split('/')[-1]
+        rel_path = 'data/' + response.meta.get('dir') + '/meta.json'
+        print("Metadata found: %s", path)
+        self.logger.info('Saving Metadata %s', path)
+        os.makedirs(os.path.dirname(rel_path), exist_ok=True)
+        with open(rel_path, 'wb') as f:
+            f.write(response.body)
 
     def save_csv(self, response):
         path = response.url.split('/')[-1]
-        rel_path = 'data/' + response.meta.get('title') + '/' + path
+        rel_path = 'data/' + response.meta.get('dir') + '/' + path
         print("CSV found: %s", path)
         self.logger.info('Saving CSV %s', path)
         os.makedirs(os.path.dirname(rel_path), exist_ok=True)
